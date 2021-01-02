dfbed76e9b0f03fd78dc4e107e4e27b35953b0f2
NitCelcius <76618755+NitCelcius@users.noreply.github.com>
2021-01-02 23:38:17 +0900

    Got POST method really working

--- bin/api.php
+++ bin/api.php
@@ -4,60 +4,56 @@
 // WIP.
 
 declare(strict_types=1);
-echo "hi";
 
-$Raw = file_get_contents("php://input");
+function ReplaceArgs(string $Basement, array $Args) {
+  return str_replace(array_keys($Args), array_values($Args), $Basement);
+}
 
 $ProfilePathFormat = "/Data/Profiles/{School_UUID}/{Group_UUID}.json";
 $TimeTablePathFormat = "/Data/Schedules/{School_UUID}/{Group_UUID}/{Year}/{Month}/{Day}/{Version}.json";
 
+$Result = array(
+  "Head" => array(
+    "Result" => false,
+    "ReasonCode" => "ERROR_UNKNOWN",
+    "ReasonText" => "The API did not respond properly to your request."
+  )
+);
 // BASICではよくある、 while(true) -> break. try~catch(exception e)~finally ができるやり方。
 while (true) {
-  $Result = array(
-    "Head" => array(
-      "Result" => false,
-      "ReasonCode" => "ERROR_UNKNOWN",
-      "ReasonText" => "The API did not respond properly to your request."
-      )
-    );
+    $Recv = json_decode(file_get_contents("php://input"), true);
 
-    $Recv = json_decode($Raw, true);
     if ($Recv === null) {
       $Result["ReasonCode"] = "INPUT_MALFORMED";
-      $Result["ReasonCode"] = "The provided JSON was malformed so the API could not recognize.";
+      $Result["ReasonText"] = "The provided JSON was malformed so the API could not recognize.";
+      break;
     }
-    var_dump($Recv);
-
+    
     //Authenticate here
     $School_UUID = $Recv["Auth"]["School_UUID"];
     $Group_UUID = $Recv["Auth"]["Group_UUID"];
     
+    // This could be request time or use requested data?
+    // Should be converted to LOCAL TIMEZONE (of school)
+    $TimeObj = new DateTime($Recv["Options"]["Date"]);
+    
+    // Revision. Must be fetched before, not constant
+    $RecentRev = 1;
+    $TimeTablePath = ReplaceArgs($TimeTablePathFormat, array(
+      "{School_UUID}" => $School_UUID,
+      "{Group_UUID}" => $Group_UUID,
+      "{Year}" => $TimeObj->format('Y'),
+      "{Month}" => $TimeObj->format('n'),
+      "{Day}" => $TimeObj->format('j'),
+      "{Version}" => $RecentRev
+    ));
+    echo "<h3>Timetable File Path</h3>";
+    echo $TimeTablePath;
+    
     break;
-  }
-  echo json_encode($Result);
+}
 
-  // This could be request time or use requested data?
-  // Should be converted to LOCAL TIMEZONE (of school)
-  $TimeObj = new DateTime();
-
-  // Revision. Must be fetched before, not constant
-  $RecentRev = 1;
-  $TimeTablePath = ReplaceArgs($TimeTablePathFormat, array(
-    "{School_UUID}" => $School_UUID,
-    "{Group_UUID}" => $Group_UUID,
-    "{Year}" => $TimeObj->format('Y'),
-    "{Month}" => $TimeObj->format('n'),
-    "{Day}" => $TimeObj->format('j'),
-    "{Version}" => $RecentRev
-  ));
-
-  echo "<h3>Timetable File Path</h3>";
-  echo $TimeTablePath;
-  echo "<br><br>";
-
-  function ReplaceArgs(string $Basement, array $Args) {
-    return str_replace(array_keys($Args), array_values($Args), $Basement);
-  }
-
-
+echo "<br>";
+echo json_encode($Result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+  
 ?>
--- tester/get_schedule.php
+++ tester/get_schedule.php
@@ -1,22 +1,26 @@
 <?php
+  $API_URL = getenv("API_URL");
+
   echo "<h1>GET_SCHEDULE test</h1>";
-  echo "<p>Request API URI: " . getenv("API_URL") . "</p>";
+  echo "<p>Request API URI: " . $API_URL . "</p>";
   echo "<h2>Body</h2>";
   echo $RawPost = file_get_contents("./test_data.json");
   // 実際はクライアントから送信されるが...テストとして json のやつを使う。
-  $post = $RawPost;
-  $api_getter = curl_init();
 
-  $Post_Obj = json_encode($post);
+  $api_getter = curl_init();
+  $post = $RawPost;
+  
+  echo "<h3>POST</h3>";
+  var_dump($post);
 
   // POST する
-
   curl_setopt_array($api_getter, [
-    CURLOPT_URL => getenv("API_URL"),
+    CURLOPT_URL => $API_URL,
     CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_POSTFIELDS => $Post_Obj
+    CURLOPT_POST => true,
+    CURLOPT_POSTFIELDS => $post,
+    CURLOPT_HTTPHEADER => array("Content-Type: application/json")
   ]);
-  curl_setopt($api_getter, CURLOPT_URL, getenv("API_URL"));
   $api_resp = curl_exec($api_getter);
 
   echo "<h2>Response</h2>";
