b54486eee567fc6b2ad969b6f87ddecb1b93e13c
NitCelcius <76618755+NitCelcius@users.noreply.github.com>
2021-01-28 23:24:58 +0900

    Swapped some lines for Tester as test reason

--- Tester/get_schedule.php
+++ Tester/get_schedule.php
@@ -2,18 +2,10 @@
   $API_URL = getenv("API_URL");
 
   $API_URL = "localhost:84/bin/api.php";
-
-  echo "<h1>GET_SCHEDULE test</h1>";
-  echo "<p>Request API URI: " . $API_URL . "</p>";
-  echo "<h2>Body</h2>";
-  echo $RawPost = file_get_contents("./test_data.json");
+  $RawPost = file_get_contents("./test_data.json");
   // 実際はクライアントから送信されるが...テストとして json のやつを使う。
-
   $api_getter = curl_init();
   $post = $RawPost;
-  
-  echo "<h3>POST</h3>";
-  var_dump($post);
 
   // POST する
   curl_setopt_array($api_getter, [
@@ -24,9 +16,18 @@
     CURLOPT_HTTPHEADER => array("Content-Type: application/json")
   ]);
   $api_resp = curl_exec($api_getter);
-
-  echo "<h2>Response</h2>";
-  echo $api_resp;
   curl_close($api_getter);
 
+  echo "<h1>GET_SCHEDULE test</h1>";
+
+  echo "<p>Request API URI: " . $API_URL . "</p>";
+  echo "<h2>Body</h2>";
+  echo $RawPost;
+
+  echo "<h3>POST</h3>";
+  var_dump($post);
+
+  echo "<h2>Response</h2>";
+
+  echo $api_resp;
 ?>
