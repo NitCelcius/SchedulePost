e128973e9c205260034654d5324a0dab06416b89
NitCelcius <76618755+NitCelcius@users.noreply.github.com>
2021-02-19 23:44:10 +0900

    index page JS now fetches personal data

--- index.js
+++ index.js
@@ -13,6 +13,46 @@ for (var i = 0; i < InterSectNodes.length; i++) {
   InterSectObs.observe(InterSectNodes[i]);
 }
 
+function FetchPersonalInfo(UserClass, CallbackSucceed, CallbackFailed, CallbackProgress) {
+  let Req = new XMLHttpRequest();
+  try {
+    Req.addEventListener("loadend", CallbackSucceed);
+    Req.addEventListener("error", CallbackFailed);
+    Req.addEventListener("progress", CallbackProgress);
+  } catch (ReferenceError) {
+    /* Do nothing */
+  }
+
+  Req.open("POST", API_URL, true);
+  Req.setRequestHeader("Content-Type", "application/json");
+
+  console.info(UserClass);
+  Req.send(JSON.stringify({
+    "Auth": {
+      "UserID": UserClass.UserID,
+      "SessionToken": UserClass.SessionToken
+    },
+    "Action": "GET_USER_PROFILE"
+  }));
+
+  return;
+}
+
+class User {
+  constructor(InUserID, InSessionToken) {
+    this.UserID = InUserID;
+    this.SessionToken = InSessionToken;
+    this.School = {
+      "ID": null,
+      "DisplayName": null
+    };
+    this.GroupID = {
+      "ID": null,
+      "DIsplayName": null
+    };
+  }
+}
+
 function Scroll_Update(Elements, Obj) {
   Elements.forEach(Node => {
     if (Node.isIntersecting === false) return;
@@ -20,17 +60,17 @@ function Scroll_Update(Elements, Obj) {
     // console.info(Node.intersectionRect);
 
     var index = InterSectIDs.indexOf(Node.target.id);
-    
-    with (Node.intersectionRect) {
 
-      console.info("index = "+index.toString()+" Ratio "+Node.intersectionRatio+" Pos "+top.toString() + "~" + bottom.toString());
+    with(Node.intersectionRect) {
+
+      console.info("index = " + index.toString() + " Ratio " + Node.intersectionRatio + " Pos " + top.toString() + "~" + bottom.toString());
       if ((Node.intersectionRatio < 0.1) && (top < 10)) {
-        index = InterSectIDs.indexOf(Node.target.id) -1;
+        index = InterSectIDs.indexOf(Node.target.id) - 1;
       }
 
       index = index >= 0 ? index : null;
     }
-        /*
+    /*
     if (Node.intersectionRatio < 0.1) {
 
       if (index >= 1) {
@@ -58,13 +98,12 @@ function Header_Update(ElementID) {
 
     default:
       Header.innerHTML = "";
-      Base_Header.forEach( Node => {
+      Base_Header.forEach(Node => {
         Header.appendChild(Node);
-      }) 
+      })
       break;
   }
 }
-
 
 /*
 var Scroller_Timer;
@@ -112,6 +151,58 @@ function Sidebar_Close() {
   document.getElementById("Nav_Overlay").style.display = "none";
 }
 
+function GetCookie(name) {
+  try {
+    // Thanks to mozilla ref, this is quite nice
+    // But this function lets every single cookie easily readable by user.
+    return document.cookie
+      .split("; ")
+      .find(Piece => Piece.startsWith(name))
+      .split("=")[1];
+  } catch (err) {
+    return null;
+  }
+}
+
+function TransferLoginPage() {
+  // NOTE: Depending on the last-update time, automatically redirect or recommend to redirect.
+  // Referer problem occurs here, but ignoring
+  console.warn("The account session token has expired. Redirecting to the sign-in page.");
+
+  OverlayDiv = document.createElement("div");
+  with(OverlayDiv.style) {
+    display = "flex"
+    position = "absolute";
+    top = 0;
+    left = 0;
+    width = "100%";
+    height = "100%";
+    zIndex = 32;
+    backgroundColor = "#fffe";
+    alignItems = "center";
+    flexDirection = "column";
+    placeContent = "center";
+  }
+  document.getElementsByTagName("Body")[0].appendChild(OverlayDiv);
+  LoadTitle = document.createElement("h1");
+  LoadTitle.innerHTML = "REDIRECTING";
+  with(LoadTitle.style) {
+    justifyContent = "center";
+    color = "#666";
+    letterSpacing = "0.3em";
+    fontSize = "1.5rem";
+    margin = 0;
+  }
+  LoadText = document.createElement("p");
+  LoadText.innerHTML = "ログインページに移動しています...";
+  with(LoadText.style) {}
+  with(OverlayDiv) {
+    appendChild(LoadTitle);
+    appendChild(LoadText);
+  }
+  location.href = "/login.html";
+}
+
 /*
 var Potato = 0;
 
@@ -121,3 +212,37 @@ setInterval(function () {
   Potato = Potato + 0.1;
 },30);
 */
+
+
+
+UserID = GetCookie("UserID");
+SessionToken = GetCookie("SessionToken");
+
+if (UserID == null || SessionToken == null) {
+  TransferLoginPage();
+}
+
+Prof = FetchPersonalInfo(new User(UserID, SessionToken),
+  function (Req) { // When succeeded
+    console.info(Req.target.responseText);
+    Resp = JSON.parse(Req.target.responseText);
+
+    if (Resp["Result"]) {
+      document.getElementById("Group_Label").innerHTML = Resp.Profile.Group.DisplayName;
+      document.getElementById("Group_Label").innerHTML = Resp.Profile.School.DisplayName;
+    } else {
+      switch (Resp["ReasonCode"]) {
+        case "ACCOUNT_SESSION_TOKEN_EXPIRED":
+        case "INVALID_CREDENTIALS": {
+          TransferLoginPage();
+          break;
+        }
+      }
+    }
+  },
+  function (Err) {
+    console.info(Err);
+  },
+  function (ProgressEvent) {
+    console.info(ProgressEvent);
+  });
